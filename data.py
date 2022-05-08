0193cf054daee5c20d33a09300674fff14c8162a
Pontus <pontus.jansson91@gmail.com>
2022-05-08 21:26:15 +0200

    Initial commit
    Initial commit, first workouts

---
+++ data.py
@@ -0,0 +1,38 @@
+workouts = {
+    'Karen': {
+        'type': 'FT',
+        'rounds_time': 1,
+        'movements': {
+            'wall balls': 150
+        }
+    },
+    'Angie': {
+        'type': 'FT',
+        'rounds_time': 1,
+        'movements': {
+            'pull-ups': 100,
+            'push-ups': 100,
+            'sit-ups': 100,
+            'squats': 100
+        }
+    },
+    'Diane': {
+        'type': 'FT',
+        'rounds_time': 3,
+        'movements': {
+            'pull-ups': 20,
+            'push-ups': 30,
+            'sit-ups': 40,
+            'squats': 50,
+            'rest': 180
+        }
+    },
+    'name': {
+        'type': 'FT or AMRAP',
+        'rounds_time': 5,
+        'movements': {
+            'övning': 'reps',
+            'övning2': 'reps'
+        }
+    }
+}
---
+++ script.py
@@ -0,0 +1,8 @@
+from data import workouts
+
+def return_crossfit_benchmark():
+    benchmark = workouts['karen']
+    return benchmark
+
+
+print(return_crossfit_benchmark())
